e37b641da022f0a552a03d5ed8ac5342af02842b
Crisoul500 <166497301+Crisoul500@users.noreply.github.com>
2025-07-22 20:08:20 -0600

    Update Insert Catalogo.sql
    Más datos de prueba para la tabla Vehiculo. Probar si con RESTORE FILELIST ONLY de la BD toma esos datos también.

--- sql/Insert Catalogo.sql
+++ sql/Insert Catalogo.sql
@@ -4,7 +4,7 @@ GO
 --Agregando Datos para los catalogos
 INSERT INTO RolUsuario (Nombre, Descripcion) VALUES
 ('Administrador', 'Usuario con permisos completos'),
-('Cliente', 'Usuario que alquila o compra vehÌculos'),
+('Cliente', 'Usuario que alquila o compra veh√≠culos'),
 ('Empleado', 'Usuario que gestiona operaciones');
 GO
 
@@ -15,7 +15,7 @@ GO
 
 INSERT INTO HorarioTrabajo (Dia, HoraInicio, HoraFin) VALUES
 ('Lunes', '08:00:00', '17:00:00'),
-('S·bado', '09:00:00', '13:00:00');
+('S√°bado', '09:00:00', '13:00:00');
 GO
 
 INSERT INTO Marca (Nombre) VALUES
@@ -26,15 +26,15 @@ INSERT INTO Marca (Nombre) VALUES
 GO
 
 INSERT INTO TipoVehiculo (NombreTipo, Descripcion) VALUES
-('Sed·n', 'VehÌculo de cuatro puertas para uso urbano'),
-('SUV', 'VehÌculo utilitario deportivo'),
-('Camioneta', 'VehÌculo de carga ligera');
+('Sed√°n', 'Veh√≠culo de cuatro puertas para uso urbano'),
+('SUV', 'Veh√≠culo utilitario deportivo'),
+('Camioneta', 'Veh√≠culo de carga ligera');
 GO
 
 INSERT INTO TipoDocumento (Nombre, Codigo) VALUES
 ('Factura', '01'),
-('Nota CrÈdito', '06'),
-('Nota DÈbito', '07'),
+('Nota Cr√©dito', '06'),
+('Nota D√©bito', '07'),
 ('Boleta Compra', '11');
 GO
 
@@ -52,14 +52,14 @@ GO
 
 INSERT INTO MetodoPago (Nombre) VALUES
 ('Efectivo'),
-('Tarjeta de CrÈdito'),
+('Tarjeta de Cr√©dito'),
 ('Transferencia Bancaria');
 GO
 
 INSERT INTO TipoExoneracion (Descripcion) VALUES
-('ExoneraciÛn Diplom·tica'),
-('ExoneraciÛn por Discapacidad'),
-('Sin ExoneraciÛn');
+('Exoneraci√≥n Diplom√°tica'),
+('Exoneraci√≥n por Discapacidad'),
+('Sin Exoneraci√≥n');
 GO
 
 INSERT INTO Pais (Nombre, Codigo_Pais) VALUES
@@ -67,43 +67,43 @@ INSERT INTO Pais (Nombre, Codigo_Pais) VALUES
 GO
 
 INSERT INTO Departamento (Nombre, Codigo_Departamento, Pais_id) VALUES
-('Atl·ntida', '01', 1),
-('ColÛn', '02', 1),
+('Atl√°ntida', '01', 1),
+('Col√≥n', '02', 1),
 ('Comayagua', '03', 1),
-('Cop·n', '04', 1),
-('CortÈs', '05', 1),
+('Cop√°n', '04', 1),
+('Cort√©s', '05', 1),
 ('Choluteca', '06', 1),
-('El ParaÌso', '07', 1),
-('Francisco Moraz·n', '08', 1),
+('El Para√≠so', '07', 1),
+('Francisco Moraz√°n', '08', 1),
 ('Gracias a Dios', '09', 1),
-('Intibuc·', '10', 1),
-('Islas de la BahÌa', '11', 1),
+('Intibuc√°', '10', 1),
+('Islas de la Bah√≠a', '11', 1),
 ('La Paz', '12', 1),
 ('Lempira', '13', 1),
 ('Ocotepeque', '14', 1),
 ('Olancho', '15', 1),
-('Santa B·rbara', '16', 1),
+('Santa B√°rbara', '16', 1),
 ('Valle', '17', 1),
 ('Yoro', '18', 1);
 GO
 
 INSERT INTO Ciudad (Nombre, Codigo_Ciudad, Departamento_id) VALUES
-('La Ceiba', '0101', 1),               -- Atl·ntida
-('Trujillo', '0201', 2),               -- ColÛn
+('La Ceiba', '0101', 1),               -- Atl√°ntida
+('Trujillo', '0201', 2),               -- Col√≥n
 ('Comayagua', '0301', 3),              -- Comayagua
-('Santa Rosa de Cop·n', '0401', 4),    -- Cop·n
-('San Pedro Sula', '0501', 5),         -- CortÈs
+('Santa Rosa de Cop√°n', '0401', 4),    -- Cop√°n
+('San Pedro Sula', '0501', 5),         -- Cort√©s
 ('Choluteca', '0601', 6),              -- Choluteca
-('Yuscar·n', '0701', 7),               -- El ParaÌso
-('Tegucigalpa', '0801', 8),            -- Francisco Moraz·n
+('Yuscar√°n', '0701', 7),               -- El Para√≠so
+('Tegucigalpa', '0801', 8),            -- Francisco Moraz√°n
 ('Puerto Lempira', '0901', 9),         -- Gracias a Dios
-('La Esperanza', '1001', 10),          -- Intibuc·
-('Roat·n', '1101', 11),                -- Islas de la BahÌa
+('La Esperanza', '1001', 10),          -- Intibuc√°
+('Roat√°n', '1101', 11),                -- Islas de la Bah√≠a
 ('La Paz', '1201', 12),                -- La Paz
 ('Gracias', '1301', 13),               -- Lempira
 ('Nueva Ocotepeque', '1401', 14),      -- Ocotepeque
 ('Juticalpa', '1501', 15),             -- Olancho
-('Santa B·rbara', '1601', 16),         -- Santa B·rbara
+('Santa B√°rbara', '1601', 16),         -- Santa B√°rbara
 ('Nacaome', '1701', 17),               -- Valle
 ('Yoro', '1801', 18);                  -- Yoro
 GO
@@ -115,16 +115,16 @@ INSERT INTO Colonia (Nombre, Ciudad_id) VALUES
 ('Santa Rosa Este', 4),
 ('San Pedro Sula Sur', 5),
 ('Choluteca Centro', 6),
-('Yuscar·n Norte', 7),
+('Yuscar√°n Norte', 7),
 ('Tegucigalpa Centro', 8),
 ('Puerto Lempira', 9),
 ('La Esperanza Oeste', 10),
-('Roat·n Sur', 11),
+('Roat√°n Sur', 11),
 ('La Paz Centro', 12),
 ('Gracias Norte', 13),
 ('Ocotepeque Este', 14),
 ('Juticalpa Centro', 15),
-('Santa B·rbara Sur', 16),
+('Santa B√°rbara Sur', 16),
 ('Nacaome Norte', 17),
 ('El Progreso Centro', 18);
 GO
@@ -132,7 +132,7 @@ GO
 INSERT INTO Permiso (Nombre, Descripcion) VALUES
 ('Seleccionar Vehiculo', 'Permite seleccionar un vehiculo'),
 ('Crear Contrato', 'Permite crear nuevos contratos'),
-('Editar VehÌculo', 'Permite modificar informaciÛn de vehÌculos');
+('Editar Veh√≠culo', 'Permite modificar informaci√≥n de veh√≠culos');
 GO
 
 INSERT INTO Modelo (Marca_id, Nombre) VALUES
@@ -152,11 +152,16 @@ INSERT INTO ParqueoVehiculo (Lote, Referencia) VALUES
 GO
 
 INSERT INTO Proveedor (NombreProveedor, RTN, ContactoNombre, Telefono, Correo, Direccion, TipoProveedor) VALUES
-('Importadora AutoHN', '08011990123458', 'Ana GÛmez', '504-9777-1234', 'contacto@autohn.com', 'Tegucigalpa', 'Distribuidor');
+('Importadora AutoHN', '08011990123458', 'Ana G√≥mez', '504-9777-1234', 'contacto@autohn.com', 'Tegucigalpa', 'Distribuidor');
 GO
 
 INSERT INTO Vehiculo (Modelo_id, Anio, VIN, Motor, MatriculaPlaca, Disponibilidad, PrecioVenta, PrecioAlquiler, TipoVehiculo_id, Estado, TipoCombustible, UsoVehiculo_id, ParqueoVehiculo_id, Proveedor_id) VALUES
-(1, 2023, '1HGCM82633A123456', '1.8L I4 DOHC 16V VVT-i', 'HND1234', 1, 250000.00, 150.00, 1, 'Nuevo', 'Gasolina', 1, 1, 1);
+(1, 2023, '1HGCM82633A123456', '1.8L I4 DOHC 16V VVT-i', 'HND1234', 1, 250000.00, 150.00, 1, 'Nuevo', 'Gasolina', 1, 1, 1),
+(7, 2018, '3G1BE6SM7JS601472', '1.5L I4 Turbo', 'HNX-4127', 1, 310000.00, 950.00, 1, 'Usado', 'Gasolina', 1, 1, 1);
+(4, 2019, '3HGGK5H57KM701893', '1.5L I4', 'HNP-2211', 1, 330000.00, 0, 2, 'Usado', 'Gasolina', 2, 2, 1),
+(8, 2022, '1G1FB1RX9N0104733', '2.3L EcoBoost', 'HNR-1189', 1, 870000.00, 0, 1, 'Nuevo', 'Gasolina', 2, 1, 1),
+(2, 2021, '3TMCZ5AN5MM374185', '3.5L V6', 'HNT-7854', 1, 790000.00, 2500.00, 3, 'Nuevo', 'Di√©sel', 2, 2, 1),
+(3, 2017, '19XFC2F5XHE045379', '1.5L I4 Turbo', 'HNM-6348', 1, 235000.00, 850.00, 1, 'Usado', 'Gasolina', 2, 2, 1);
 GO
 
 --Insert de un Empleado
@@ -166,7 +171,7 @@ GO
 SELECT * FROM Direccion;
 
 INSERT INTO Persona (PrimerNombre, SegundoNombre, PrimerApellido, SegundoApellido, Telefono, Direccion_id, Sexo) VALUES
-('MarÌa', 'Luisa', 'RodrÌguez', 'LÛpez', '504-9888-5678', 2, 'F');
+('Mar√≠a', 'Luisa', 'Rodr√≠guez', 'L√≥pez', '504-9888-5678', 2, 'F');
 GO
 
 INSERT INTO Usuario (Correo, Contrasenia, InicioUso, FinUso, Rol_id, Persona_id) VALUES
